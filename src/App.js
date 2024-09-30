979eceea899cb71472028e8050f82cc4e04ff44c
Paulo Souza <paulo.souza@kuehne-nagel.com>
2024-09-30 11:07:37 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import MyForm from './components/MyForm';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Forms</h2>
+      <MyForm user={{name: "Josias", email: "josias@gmail.com", bio:"Eu sou um editor.", role: "Editor"}}/>
     </div>
   );
 }
